262e4f4f34d8c2eb94353a232712d69086b51e7f
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-11-01 13:56:39 +0100

    presentatie

--- src/lib/cosmic.js
+++ src/lib/cosmic.js
@@ -1,16 +1,21 @@
-import Cosmic from "cosmicjs";
-const api = Cosmic();
 
-const bucket = api.bucket({
-  slug: import.meta.env.PUBLIC_COSMIC_BUCKET_SLUG,
-  read_key: import.meta.env.PUBLIC_COSMIC_READ_KEY,
-});
+import { createBucketClient } from '@cosmicjs/sdk'
 
+//Bucket aanmaken
+const cosmic = createBucketClient({
+  bucketSlug: import.meta.env.PUBLIC_COSMIC_BUCKET_SLUG,
+  readKey: import.meta.env.PUBLIC_COSMIC_READ_KEY
+})
+
+
+// Data ophalen
 export async function getAllPosts() {
-    const data = await bucket.objects
-      .find({
-        type: "posts",
-      })
-      .props("title,slug,content,metadata")
-    return data.objects;
-  }
+  const data = await cosmic.objects
+    .find({
+      type: 'posts'
+    })
+    .props('title,slug,metadata,created_at,url,beschrijving')
+  return data.objects
+}
+
+
